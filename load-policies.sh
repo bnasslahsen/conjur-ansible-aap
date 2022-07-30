97e998a13425def9a0e08c8c28aea4ae97dea7c8
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-07-30 21:06:56 +0200

    update

--- load-policies.sh
+++ load-policies.sh
@@ -8,4 +8,4 @@ set +a
 conjur policy update -b root -f ansible-host.yml
 conjur policy update -b root -f ansible-secrets.yml
 
-conjur variable set -i cd/ansible/secrets/github_username -v "$GIT_USER"
+conjur variable set -i cd/ansible/secrets/nginx_private_key -v "toto"
